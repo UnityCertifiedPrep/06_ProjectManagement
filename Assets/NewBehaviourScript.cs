fd883b4b30452152cc7a38c2fdac780c01fd87ad
Sam Pattuzzi <sam.pattuzzi@googlemail.com>
2016-06-01 12:50:49 +0100

    03 GameObjects » Tags

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var cubes = GameObject.FindGameObjectsWithTag("Cube");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
